b8ed735cb57b2a573191b08d812e6f9baf6aa5ed
Krzysztof Radzięta <kustosz142@gmail>
2024-02-03 14:29:18 +0100

    added background when nav is activated

--- components/aside-nav.tsx
+++ components/aside-nav.tsx
@@ -13,25 +13,33 @@ const AsideNavigation: React.FC<AsideNavigationProps> = ({
   setIsAsideNavigationVisible,
 }) => {
   return (
-    <aside
-      className={`fixed z-50 h-screen bg-black ${
-        isAsideNavigationVisible ? "w-96" : "w-0"
-      } right-0 text-white flex flex-col overflow-hidden transition-all`}
-    >
-      <div className="flex justify-between items-center border-b-2 border-custom-blue">
-        <h3 className="uppercase text-2xl md:text-3xl ">Menu</h3>
-        <button onClick={() => setIsAsideNavigationVisible((prev) => !prev)}>
-          <CloseOutlined className="text-3xl md:text-4xl transition duration-200 ease-in-out hover:scale-125" />
-        </button>
-      </div>
-      <nav>
-        <ul>
-          <li>LINK</li>
-          <li>LINK</li>
-          <li>LINK</li>
-        </ul>
-      </nav>
-    </aside>
+    <>
+      {isAsideNavigationVisible && (
+        <div
+          className="fixed inset-0 bg-black bg-opacity-50 z-40"
+          onClick={() => setIsAsideNavigationVisible(false)}
+        />
+      )}
+      <aside
+        className={`${
+          isAsideNavigationVisible ? "w-96" : "w-0"
+        } fixed z-50 h-screen bg-black  right-0 text-white flex flex-col overflow-hidden transition-all duration-300`}
+      >
+        <div className="flex justify-between items-center border-b-2 border-custom-blue py-4 px-2">
+          <h3 className="uppercase text-2xl md:text-3xl ">Menu</h3>
+          <button onClick={() => setIsAsideNavigationVisible((prev) => !prev)}>
+            <CloseOutlined className="text-3xl md:text-4xl transition duration-200 ease-in-out hover:scale-125" />
+          </button>
+        </div>
+        <nav>
+          <ul>
+            <li>LINK</li>
+            <li>LINK</li>
+            <li>LINK</li>
+          </ul>
+        </nav>
+      </aside>
+    </>
   );
 };
 
--- components/main-header.tsx
+++ components/main-header.tsx
@@ -36,7 +36,7 @@ const MainHeader = () => {
   return (
     <>
       <header
-        className={`fixed top-0 z-50 w-full flex justify-between items-center px-4 md:px-16 py-2 bg-white transition-transform duration-200 ease-in-out ${
+        className={`fixed top-0 z-30 w-full flex justify-between items-center px-4 md:px-16 py-2 bg-white transition-transform duration-200 ease-in-out ${
           isHeaderVisible ? "translate-y-0" : "-translate-y-full"
         }`}
       >
