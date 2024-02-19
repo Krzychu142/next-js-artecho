c5c3d97e4820c016c51ae0c29379d3f43dfcce6a
Krzysztof Radzięta <kustosz142@gmail>
2024-02-20 00:55:44 +0100

    moved show/hide icon's classes to const variable

--- components/signin/signin-form-password-input.tsx
+++ components/signin/signin-form-password-input.tsx
@@ -4,6 +4,9 @@ import { EyeInvisibleOutlined, EyeOutlined } from "@ant-design/icons";
 import { formElementClasses } from "./signin-form";
 import { useState } from "react";
 
+const iconClasses =
+  "absolute right-3 top-1/2 transform -translate-y-1/2 text-lg cursor-pointer text-custom-blue hover:text-black transition duration-200 ease-in-out";
+
 const SignInFormPasswordInput = () => {
   const [showPassword, setShowPassword] = useState<boolean>(false);
 
@@ -21,14 +24,14 @@ const SignInFormPasswordInput = () => {
 
       {showPassword ? (
         <EyeInvisibleOutlined
-          className="absolute right-3 top-1/2 transform -translate-y-1/2 text-lg cursor-pointer text-custom-blue hover:text-black transition duration-200 ease-in-out"
+          className={iconClasses}
           title="Hide password"
           role="button"
           onClick={togglePasswordVisibility}
         />
       ) : (
         <EyeOutlined
-          className="absolute right-3 top-1/2 transform -translate-y-1/2 text-lg cursor-pointer text-custom-blue hover:text-black transition duration-200 ease-in-out"
+          className={iconClasses}
           title="Show password"
           role="button"
           onClick={togglePasswordVisibility}
